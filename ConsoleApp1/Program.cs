8c441969dc3fc2e021b7489c28755e08cc8cbeb4
Arindam Dey <arindamdeyofficial@gmail.com>
2023-07-19 11:27:12 +0530

    initial

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] arr = new int[] {1,2,2,5,5 };
+//int[] arrF = new int[] { };
+for (int i=0; i< arr.Length; i++)
+{
+    bool isDuplicate = false;
+    for (int a = 0; a < i; a++)
+    {
+        isDuplicate = arr[a] == arr[i];        
+    }    
+    if(!isDuplicate)
+    {
+        //arrF.Append(arr[i]);
+        Console.WriteLine(arr[i]);
+    }
+}
+
+
